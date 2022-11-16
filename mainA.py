e7ac1247858ce884d925ff4fcdc6e9de9d17577d
ruscelli-riccardo <ruscelli.riccardo.angelo@iisgalvanimi.edu.it>
2022-11-16 12:00:28 +0000

    primaversione

---
+++ mainA.py
@@ -0,0 +1,4 @@
+for x in range(5):
+    numero = input("inserire numero:")
+    numero = int(numero)
+    print(numero)
---
+++ mainB.py
@@ -0,0 +1,7 @@
+lista=[]
+for x in range(5):
+    numero = input("inserire numero:")
+    numero = int(numero)
+    lista.append(numero)
+lista.reverse()
+print(lista)
